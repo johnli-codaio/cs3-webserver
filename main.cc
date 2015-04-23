e56d765a4b263e0873065a93bb231959e1fe0238
Connor Crowley <ccrowley@caltech.edu>
2015-04-23 15:18:06 -0700

    Added #include to make file compile

--- main.cc
+++ main.cc
@@ -1,3 +1,6 @@
+// Based on code from Synchronous TCP Daytime Server
+// at http://www.boost.org/doc/libs/1_57_0/doc/html/boost_asio/tutorial/tutdaytime2.html
+
 #include <ctime>
 #include <iostream>
 #include <string>
@@ -14,19 +17,18 @@ std::string make_daytime_string() {
 int main() {
   try {
     boost::asio::io_service io_service;
-
-	tcp::acceptor acceptor(io_service, tcp::endpoint(tcp::v4(), 13));
-
+    
+    tcp::acceptor acceptor(io_service, tcp::endpoint(tcp::v4(), 13));
+    
     while (true) {
       tcp::socket socket(io_service);
-      acceptor.accept(socket);
-
-      std::string message = make_daytime_string();
+      acceptor.accept(socket);     std::string message = make_daytime_string();
 
       boost::system::error_code ignored_error;
       boost::asio::write(socket, boost::asio::buffer(message), ignored_error);
     }
   }
+  
   catch (std::exception& e) {
     std::cerr << e.what() << std::endl;
   }
