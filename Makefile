1c3073a08a1b0a7a077fc166471cf1b5420e2674
Jack <jack4440@caltech.edu>
2015-04-23 20:13:35 -0700

    Cleaned up code from boost asio tutorial. Implemented feature to reading config file and set port. Updated make file accordingly.

--- Makefile
+++ Makefile
@@ -1,9 +1,13 @@
 all: main
 
 clean: 
-	rm -rf *.o *~ testunacceptable testmyalloc \
-		testunacceptable.exe testmyalloc.exe
+	rm -rf *.o *~ 
+
+config_parser.o: config_parser.cc
+	g++ -Wall -g -c -std=c++0x config_parser.cc
 
 main.o:	main.cc
-main:	main.o
-	g++ -std=c++0x -g -Wall main.cc -o main -lboost_system
+	g++ -Wall -std=c++0x -g -Wall -c main.cc
+
+main:	main.o config_parser.o
+	g++ -std=c++0x -g -Wall main.o config_parser.o -o main -lboost_system
--- main.cc
+++ main.cc
@@ -5,34 +5,63 @@
 #include <iostream>
 #include <string>
 #include <boost/asio.hpp>
+#include "config_parser.h"
 
 using boost::asio::ip::tcp;
 
-std::string make_hello_world() {
-  using namespace std; // For time_t, time and ctime;
-  string ret = "<html><body>Hello, world!</body></html>";
-  return ret;
+const std::string message = "<html><body>Hello, world!</body></html>";
+
+// basically a dfs tree search
+int getPort(NginxConfig config) {
+    int port = -1;
+    for (const auto& statement : config.statements_){
+        bool listen = false;
+        for (std::string token : statement->tokens_){
+            if (listen){
+                int port = std::stoi(token);
+                return port;
+            }
+            else if (token == "listen"){
+                listen = true;
+            }
+        }
+        if ((statement->child_block_).get() != nullptr) {
+            port = getPort(*(statement->child_block_));
+        }
+        if (port > 0) {
+            return port;
+        }
+    }
+    return port;
 }
 
-int main() {
-  try {
-    boost::asio::io_service io_service;
-    
-    tcp::acceptor acceptor(io_service, tcp::endpoint(tcp::v4(), 35000));
-    
-    while (true) {
-      tcp::socket socket(io_service);
-      acceptor.accept(socket);
-      std::string message = make_hello_world();
-
-      boost::system::error_code ignored_error;
-      boost::asio::write(socket, boost::asio::buffer(message), ignored_error);
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+    printf("Usage: ./config_parser <path to config file>\n");
+    return 1;
     }
-  }
+    NginxConfigParser config_parser;
+    NginxConfig config;
+    config_parser.Parse(argv[1], &config);
+    int port = getPort(config);
+    std::cout << "Port: " << port << '\n';
+    try {
+        boost::asio::io_service io_service;
+    
+        tcp::acceptor acceptor(io_service, tcp::endpoint(tcp::v4(), port));
+    
+        while (true) {
+          tcp::socket socket(io_service);
+          acceptor.accept(socket);
+          boost::system::error_code ignored_error;
+          boost::asio::write(socket, boost::asio::buffer(message), 
+            ignored_error);
+        }
+    }
   
-  catch (std::exception& e) {
-    std::cerr << e.what() << std::endl;
-  }
+    catch (std::exception& e) {
+        std::cerr << e.what() << std::endl;
+    }
 
-  return 0;
+    return 0;
 }
