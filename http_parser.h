e81f7c101c1d45943ec83b91a4972edf12928d66
Jack Wang <j.wang@caltech.edu>
2015-05-21 06:16:42 -0700

    missing include

--- http_parser.h
+++ http_parser.h
@@ -1,6 +1,8 @@
 #ifndef _HTTP_PARSER_INCLUDED
 #define _HTTP_PARSER_INCLUDED
 
+#include "reques_handler.h"
+
 HTTPRequest parseRequest(std::string request);
 std::string unparseRequest(HTTPRequest request);
 
